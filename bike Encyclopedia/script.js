b66849a261f841da37c94fec2b85fbaa13de5c53
mohamed alfan <alfanmohamed91@gmail.com>
2024-09-08 20:12:42 +0530

    Add files via upload

---
+++ bike Encyclopedia/script.js
@@ -0,0 +1,195 @@
+var popupoverlay = document.querySelector(".popup-overlay")
+var popupbox = document.querySelector(".popup-box")
+var addpopupbutton = document.getElementById("popupup")
+addpopupbutton.addEventListener("click",function(){
+    popupoverlay.style.display="block"
+    popupbox.style.display="block"
+})
+
+var cancelpopup = document.getElementById("cancel-popup")
+cancelpopup.addEventListener("click",function(){
+    
+     popupoverlay.style.display="none"
+    popupbox.style.display="none"
+
+})
+var popupboxss = document.querySelector(".popup-boxss")
+var addpopupbuttonss = document.getElementById("popupupss")
+addpopupbuttonss.addEventListener("click",function(){
+    popupoverlay.style.display="block"
+    popupboxss.style.display="block"
+})
+
+var cancelpopupss = document.getElementById("cancel-popupss")
+cancelpopupss.addEventListener("click",function(){
+    
+     popupoverlay.style.display="none"
+    popupboxss.style.display="none"
+
+})
+
+
+var popupboxtn = document.querySelector(".popup-boxtn")
+var addpopupbuttontn = document.getElementById("popupuptn")
+addpopupbuttontn.addEventListener("click",function(){
+    popupoverlay.style.display="block"
+    popupboxtn.style.display="block"
+})
+
+var cancelpopuptn = document.getElementById("cancel-popuptn")
+cancelpopuptn.addEventListener("click",function(){
+    
+     popupoverlay.style.display="none"
+    popupboxtn.style.display="none"
+
+})
+
+
+var popupboxhc = document.querySelector(".popup-boxhc")
+var addpopupbuttonhc = document.getElementById("popupuphc")
+addpopupbuttonhc.addEventListener("click",function(){
+    popupoverlay.style.display="block"
+    popupboxhc.style.display="block"
+})
+
+var cancelpopuphc = document.getElementById("cancel-popuphc")
+cancelpopuphc.addEventListener("click",function(){
+    
+     popupoverlay.style.display="none"
+    popupboxhc.style.display="none"
+
+})
+
+
+
+var popupboxmt = document.querySelector(".popup-boxmt")
+var addpopupbuttonmt = document.getElementById("popupupmt")
+addpopupbuttonmt.addEventListener("click",function(){
+    popupoverlay.style.display="block"
+    popupboxmt.style.display="block"
+})
+
+var cancelpopupmt = document.getElementById("cancel-popupmt")
+cancelpopupmt.addEventListener("click",function(){
+    
+     popupoverlay.style.display="none"
+    popupboxmt.style.display="none"
+
+})
+
+
+var popupboxrr = document.querySelector(".popup-boxrr")
+var addpopupbuttonrr = document.getElementById("popupuprr")
+addpopupbuttonrr.addEventListener("click",function(){
+    popupoverlay.style.display="block"
+    popupboxrr.style.display="block"
+})
+
+var cancelpopuprr = document.getElementById("cancel-popuprr")
+cancelpopuprr.addEventListener("click",function(){
+    
+     popupoverlay.style.display="none"
+    popupboxrr.style.display="none"
+
+})
+
+
+var popupboxla = document.querySelector(".popup-boxla")
+var addpopupbuttonla = document.getElementById("popupupla")
+addpopupbuttonla.addEventListener("click",function(){
+    popupoverlay.style.display="block"
+    popupboxla.style.display="block"
+})
+
+var cancelpopupla = document.getElementById("cancel-popupla")
+cancelpopupla.addEventListener("click",function(){
+    
+     popupoverlay.style.display="none"
+    popupboxla.style.display="none"
+
+})
+
+
+var popupboxfv = document.querySelector(".popup-boxfv")
+var addpopupbuttonfv = document.getElementById("popupupfv")
+addpopupbuttonfv.addEventListener("click",function(){
+    popupoverlay.style.display="block"
+    popupboxfv.style.display="block"
+})
+
+var cancelpopupfv = document.getElementById("cancel-popupfv")
+cancelpopupfv.addEventListener("click",function(){
+    
+     popupoverlay.style.display="none"
+    popupboxfv.style.display="none"
+
+})
+
+
+
+var popupboxtm = document.querySelector(".popup-boxtm")
+var addpopupbuttontm = document.getElementById("popupuptm")
+addpopupbuttontm.addEventListener("click",function(){
+    popupoverlay.style.display="block"
+    popupboxtm.style.display="block"
+})
+
+var cancelpopuptm = document.getElementById("cancel-popuptm")
+cancelpopuptm.addEventListener("click",function(){
+    
+     popupoverlay.style.display="none"
+    popupboxtm.style.display="none"
+
+})
+
+
+var popupboxbv = document.querySelector(".popup-boxbv")
+var addpopupbuttonbv = document.getElementById("popupupbv")
+addpopupbuttonbv.addEventListener("click",function(){
+    popupoverlay.style.display="block"
+    popupboxbv.style.display="block"
+})
+
+var cancelpopupbv = document.getElementById("cancel-popupbv")
+cancelpopupbv.addEventListener("click",function(){
+    
+     popupoverlay.style.display="none"
+    popupboxbv.style.display="none"
+
+})
+
+
+var popupboxbmw = document.querySelector(".popup-boxbmw")
+var addpopupbuttonbmw = document.getElementById("popupupbmw")
+addpopupbuttonbmw.addEventListener("click",function(){
+    popupoverlay.style.display="block"
+    popupboxbmw.style.display="block"
+})
+
+var cancelpopupbmw = document.getElementById("cancel-popupbmw")
+cancelpopupbmw.addEventListener("click",function(){
+    
+     popupoverlay.style.display="none"
+    popupboxbmw.style.display="none"
+
+})
+
+
+
+var popupboxbmc = document.querySelector(".popup-boxbmc")
+var addpopupbuttonbmc = document.getElementById("popupupbmc")
+addpopupbuttonbmc.addEventListener("click",function(){
+    popupoverlay.style.display="block"
+    popupboxbmc.style.display="block"
+})
+
+var cancelpopupbmc = document.getElementById("cancel-popupbmc")
+cancelpopupbmc.addEventListener("click",function(){
+    
+     popupoverlay.style.display="none"
+    popupboxbmc.style.display="none"
+
+})
+
+
+
